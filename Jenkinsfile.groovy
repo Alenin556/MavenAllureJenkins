b0156b6a09586aadf142620b8701cc34522be284
a.alenin <a.alenin@astondevs.ru>
2024-04-17 14:19:27 +0300

    add groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,70 +1,22 @@
-task_branch = "${TEST_BRANCH_NAME}"
-def branch_cutted = task_branch.contains("origin") & task_branch.split('/')[1]; task_branch.trim()
-currentBuild.displayName = "$branch_cutted"
-base_git_url = "https://github.com/Alenin556/MavenAllureJenkins.git"
+#!groovy
 
-node {
-    withEnv(["branch = ${branch_cutted}", "base_url=${base_git_url}"]) {
-        stage("Checkout Branch") {
-            if (!"$branch_cutted".contains("master")) {
-                try {
-                    getProject("$base_git_url", "$branch_cutted")
-                } catch (err) {
-                    echo "Failed get Branch $branch_cutted"
-                    throw ("$err") as Throwable
-                }
-            } else {
-                echo "Current branch master"
+pipeline {
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'echo "This is my first step"'
             }
         }
-
-        try{
-            stage("Run tests") {
-                parallel(
-                        'Smoke' : {
-                            runTestWithTag("smoke")
-                        },
-                        'Regres' : {
-                            runTestWithTag("regres")
-                        }
-                )
-            }
-        } finally {
-            stage ("Report") {
-                getAllureReport()
-                    }
+        stage('Test') {
+            steps
+            sh 'echo "This is my Test step"'
         }
     }
-
-
-}
-
-def runTestWithTag(String tag) {
-    try {
-        labelledShell(label : "Run ${tag}", script: "mvn test -D groups=${tag}")
-    } finally {
-        echo "test run finish"
+    stage('Deploy') {
+        steps {
+            sh 'echo "This is my Deploy step"'
+        }
     }
 }
 
-
-def getProject(String repo, String branch) {
-    cleanWs()
-    checkout scm: [
-            $class           : 'GitSCM', branches: [[name: branch]],
-            userRemoteConfigs: [[
-                                        url: repo
-                                ]]
-    ]
-}
-
-def getAllureReport() {
-    allure([
-            includePropeties: true,
-            jdk: '',
-            properties: [],
-            reportBuildPolicy: 'ALWAYS',
-            results: [[path: "target/allure-results"]],
-    ])
-
-}
