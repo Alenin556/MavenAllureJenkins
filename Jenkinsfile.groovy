a0a6bc3abfdaa5f6e5f2e23ead149a8f803ca0f9
a.alenin <a.alenin@astondevs.ru>
2024-04-17 14:20:48 +0300

    add groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,22 +1,7 @@
 #!groovy
 
-pipeline {
-
-    stages {
-        stage('Build') {
-            steps {
-                sh 'echo "This is my first step"'
-            }
-        }
-        stage('Test') {
-            steps
-            sh 'echo "This is my Test step"'
-        }
-    }
-    stage('Deploy') {
-        steps {
-            sh 'echo "This is my Deploy step"'
-        }
+node { // node/agent
+    stage('Stage 1') {
+        echo 'Hello World' // echo Hello World
     }
 }
-
